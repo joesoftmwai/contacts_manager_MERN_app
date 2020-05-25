4c237a69bc81227fae9a5cb0aba44376bfd3afd1
Mwai Joseph <=>
2020-05-25 07:33:35 +0300

    validating the user model as well as encrypting the password for user with bcrypt

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const app = express();
 // Connecct Database
 connectDB();
 
+// Init middleware
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) =>
   res.json({ msg: 'Welcome to the ContactsManager API ..' })
 );
